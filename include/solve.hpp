fd79027d8085e3d1a2b31482df167bac742240e3
Shulgin Konstantin <68589028+Shulliikkk@users.noreply.github.com>
2025-09-09 03:05:07 +0300

    heat coduction and LLF scheme

--- include/solve.hpp
+++ include/solve.hpp
@@ -2,6 +2,7 @@
 #include "discharge.hpp"
 #include <Eigen/Dense>
 #include <fstream>
+#include <utility>
 
 class Solver: protected Discharge{
     private:
@@ -12,26 +13,33 @@ class Solver: protected Discharge{
 
         void set_init_conditions(double n_i_min, double n_i_max, double T_e_anode_eV, double T_e_cathode_eV);
 
-        Plasma_state right_boundary_condition(std::size_t time_ind);
+        Plasma_state right_boundary_condition(const std::size_t time_ind) const;
 
-        Plasma_state left_boundary_condition(std::size_t time_ind);
-        
-        Electrons_state calc_electrons_state(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        Plasma_state left_boundary_condition(const std::size_t time_ind) const;
 
-        Eigen::MatrixXd Omega_L(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        Eigen::VectorXd hyperbolic_system_flux(const std::size_t time_ind, const Plasma_state curr_state) const;
 
-        Eigen::MatrixXd Omega_R(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        Eigen::VectorXd hyperbolic_numerical_flux(const std::size_t time_ind, const Plasma_state left_state, const Plasma_state right_state) const;
 
-        Eigen::MatrixXd Lamda_plus(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        Eigen::VectorXd hyperbolic_right_hand_side(const std::size_t time_ind, const std::size_t spatial_ind) const;
 
-        Eigen::MatrixXd Lamda_minus(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        Eigen::VectorXd tridiagonal_matrix_algorithm(const Eigen::VectorXd& left_secondary_diagonal, const Eigen::VectorXd& main_diagonal, const Eigen::VectorXd& right_secondary_diagonal, const Eigen::VectorXd& column) const;
 
-        Eigen::MatrixXd A_plus(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        double electron_cross_field_mobility(const Plasma_state state, const double spatial_ind) const;
 
-        Eigen::MatrixXd A_minus(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        std::pair<double, double> heat_flux_coefficient_plus(const std::size_t time_ind, const std::size_t spatial_ind, const double T_e_curr, const double T_e_next) const;
 
-        Eigen::VectorXd right_hand_side(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        std::pair<double, double> heat_flux_coefficient_minus(const std::size_t time_ind, const std::size_t spatial_ind, const double T_e_curr, const double T_e_prev) const;
 
-        std::vector<std::vector<Plasma_state>> solve();
+        double flux_coefficient(const std::size_t time_ind, const std::size_t spatial_ind) const;
 
+        double upwind_coefficient_plus(const std::size_t time_ind, const std::size_t spatial_ind) const;
+
+        double upwind_coefficient_minus(const std::size_t time_ind, const std::size_t spatial_ind) const;
+
+        std::pair<std::array<Eigen::VectorXd, 3>, Eigen::MatrixXd> Jacobian_matrix(const std::size_t time_ind, const Eigen::VectorXd vec_T_e) const;
+
+        Eigen::VectorXd nonlin_equation_function(const std::size_t time_ind, const Eigen::VectorXd T_e_vec) const;
+
+        std::vector<std::vector<Plasma_state>> solve(const double eps = 0.5, const size_t max_iters = 100);
 };
--- include/walls.hpp
+++ include/walls.hpp
@@ -8,7 +8,7 @@ class Walls
     protected:
         std::vector<std::vector<Plasma_state>> states;
     public:
-        double SEE_coef(const std::size_t time_ind, const std::size_t spatial_ind) const;
+        double SEE_coef(const Plasma_state curr_state) const;
 
         double ions_wall_coll_friq(const std::size_t time_ind, const size_t spatial_ind) const;
 
